4decb97c11d210d0dc711a5665feb953ca6dc303
Freek van de Ven <FJM.vandeVen@student.han.nl>
2022-08-04 17:19:30 +0200

    feat: riverpod provider for services

--- lib/src/providers.dart
+++ lib/src/providers.dart
@@ -1,1 +1,24 @@
+import 'package:beer_distribution_game/src/models/models.dart';
+import 'package:beer_distribution_game/src/services/services.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+/* provide implementations of the service interfaces */
+
+final gameProvider = StateNotifierProvider<GameService, BeerGame>(
+  (ref) => BeerGameService(),
+);
+
+final browserProvider =
+    StateNotifierProvider<GameBrowserService, List<GameInfo>>(
+  (ref) => BeerGameBrowserService(),
+);
+
+final gameConfigProvider =
+    StateNotifierProvider<GameConfigService, GameConfiguration>(
+  (ref) => BeerGameConfigService(),
+);
+
+final applicationConfigProvider = StateNotifierProvider<
+    ApplicationConfigurationService, ApplicationConfiguration>(
+  (ref) => BeerApplicationConfigurationService(),
+);
