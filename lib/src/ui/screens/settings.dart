de9cd45646f13df316fe96882b324076f28746ca
Freek van de Ven <FJM.vandeVen@student.han.nl>
2022-08-12 11:13:29 +0200

    feat: style settingscreen

--- lib/src/ui/screens/settings.dart
+++ lib/src/ui/screens/settings.dart
@@ -3,6 +3,7 @@ import 'package:beer_distribution_game/src/models/models.dart';
 import 'package:beer_distribution_game/src/providers.dart';
 import 'package:beer_distribution_game/src/services/services.dart';
 import 'package:beer_distribution_game/src/ui/screens/base.dart';
+import 'package:beer_distribution_game/src/ui/widgets/buttons/primary_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -21,13 +22,26 @@ class SettingScreen extends ConsumerWidget {
     var lang = Localizations.localeOf(context);
     var translate = AppLocalizations.of(context)!;
     return BaseScreen(
-      child: Center(
+      child: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xFFe8e9eb),
+              Color(0xFFc5cad6),
+            ],
+          ),
+        ),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              translate.settings_title,
-              style: Theme.of(context).textTheme.headline3,
+            Padding(
+              padding: const EdgeInsets.only(top: 100.0, bottom: 10),
+              child: Text(
+                translate.settings_title,
+                style: Theme.of(context).textTheme.headline1,
+              ),
             ),
             Text(
               translate.settings_language,
@@ -53,45 +67,61 @@ class SettingScreen extends ConsumerWidget {
                   )
                   .toList(),
             ),
-            GestureDetector(
-              onTap: () async {
-                // show a confirmation dialog
-                if (await showDialog(
-                  context: context,
-                  builder: (context) => AlertDialog(
-                    title: Text(translate.settings_clear_confirm),
-                    actions: [
-                      TextButton(
-                        onPressed: () => Navigator.of(context).pop(false),
-                        child: Text(translate.buttonCancel),
-                      ),
-                      TextButton(
-                        onPressed: () => Navigator.of(context).pop(true),
-                        child: Text(translate.buttonConfirm),
-                      ),
-                    ],
-                  ),
-                )) {
-                  var prefs = await SharedPreferences.getInstance();
-                  await prefs.clear();
-                  await ref
-                      .read(applicationConfigProvider.notifier)
-                      .saveApplicationSettings(
-                        ApplicationConfiguration.defaultConfiguration(),
-                      );
-                  // show a success message
-                }
-              },
-              child: Text(
-                translate.settings_clear,
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20.0),
+              child: PrimaryButton(
+                onPressed: () async {
+                  // show a confirmation dialog
+                  if (await showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: Text(translate.settings_clear_confirm),
+                      actions: [
+                        TextButton(
+                          onPressed: () => Navigator.of(context).pop(false),
+                          child: Text(translate.buttonCancel),
+                        ),
+                        TextButton(
+                          onPressed: () => Navigator.of(context).pop(true),
+                          child: Text(translate.buttonConfirm),
+                        ),
+                      ],
+                    ),
+                  )) {
+                    var prefs = await SharedPreferences.getInstance();
+                    await prefs.clear();
+                    await ref
+                        .read(applicationConfigProvider.notifier)
+                        .saveApplicationSettings(
+                          ApplicationConfiguration.defaultConfiguration(),
+                        );
+                    // show a success message
+                  }
+                },
+                child: Text(
+                  translate.settings_clear,
+                  style: Theme.of(context).textTheme.headline3,
+                ),
               ),
             ),
-            GestureDetector(
-              onTap: () {
+            PrimaryButton(
+              onPressed: () {
                 Navigator.of(context).pop();
               },
-              child: Text(
-                translate.home_back,
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(
+                    Icons.arrow_back_rounded,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    translate.home_back,
+                    style: Theme.of(context).textTheme.headline4,
+                  ),
+                ],
               ),
             ),
           ],
